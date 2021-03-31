ffe00b056c549027484985b55cbad804fa9407a8
barb-code <barbara.souza.pe96@gmail.com>
2021-03-31 10:19:18 -0300

    Criando servidor

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const express = require('express')
-
+const { request, response } = require('express');
+const express  = require('express'); 
 const app = express();
 
-console.log(app);
+// console.log(app);
+app.get('/',(request,response) => {
+    response.send('Hello, world!');
+})
+
+app.listen(3000, () => {
+    console.log('Servidor rodando!');
+})
